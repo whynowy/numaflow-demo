3c300f638ad057120948bb8fd2392a74c9f1b6c8
Nandita Koppisetty <nandita.iitkgp@gmail.com>
2023-11-02 15:24:29 -0700

    fix: postprocess
    Signed-off-by: Nandita Koppisetty <nandita.iitkgp@gmail.com>

--- sentiment-analysis/udf.py
+++ sentiment-analysis/udf.py
@@ -6,8 +6,26 @@ import logging
 from pynumaflow.mapper import Messages, Message, Datum, Mapper
 from transformers import pipeline
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def postprocess(keys: list[str], datum: Datum) -> Messages:
+    output = json.loads(datum.value.decode("utf-8"))
+    messages = Messages()
+    if len(output) == 0:
+        messages.append(Message.to_drop())
+        return messages
+    logger.info("%s - Received msg: %s", output['id'], output)
+    sentiment = output['sentiment']
+    logger.info("%s - Sending msg to %s sink", output['id'], sentiment)
+    output = json.dumps(output).encode("utf-8")
+    messages.append(Message(keys=keys, value=output, tags=[sentiment]))
+    return messages
+
 
 class SentimentAnalyzer:
+
     def __init__(self):
         self.analyzer = pipeline("sentiment-analysis")
 
@@ -19,42 +37,26 @@ class SentimentAnalyzer:
             return messages
 
         output = {'id': str(uuid.uuid4())}
-        logging.info("%s - Received msg: %s", output['id'], strs)
+        logger.info("%s - Received msg: %s", output['id'], strs)
 
         sentiment = self.analyzer(strs)
         output['text'] = strs
         output['sentiment'] = sentiment[0]['label']
 
-        logging.info("%s - Sentiment analysis: %s", output['id'], output)
+        logger.info("%s - Sentiment analysis: %s", output['id'], output)
         output = json.dumps(output).encode("utf-8")
         messages.append(Message(output, keys=keys))
-        return messages
-
-    @staticmethod
-    def postprocess(keys: list[str], datum: Datum) -> Messages:
-        output = datum.value.decode("utf-8")
-        messages = Messages()
-        if len(output) == 0:
-            messages.append(Message.to_drop())
-            return messages
-
-        logging.info("%s - Received msg: %s", output['id'], output)
-        sentiment = output['sentiment']
-        logging.info("%s - Sending msg to %s sink", output['id'], sentiment)
-
-        output = json.dumps(output).encode("utf-8")
-        messages.append(Message(keys=keys, value=output, tags=[sentiment]))
         return messages
 
 
 if __name__ == "__main__":
     handler = sys.argv[1]
-    logging.info("Starting handler: %s", handler)
+    logger.info("Starting handler: %s", handler)
 
-    sa = SentimentAnalyzer()
     if handler == "inference":
+        sa = SentimentAnalyzer()
         grpc_server = Mapper(handler=sa.inference)
         grpc_server.start()
     elif handler == "postprocess":
-        grpc_server = Mapper(handler=sa.postprocess)
+        grpc_server = Mapper(handler=postprocess)
         grpc_server.start()
